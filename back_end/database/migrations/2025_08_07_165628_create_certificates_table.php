afd78739ee39ef7878a96d73ffdcbb1ff8a0a068
Natasa Matovic <nacamatovic47@gmail.com>
2025-08-07 18:57:18 +0200

    Migracija Certificates

---
+++ back_end/database/migrations/2025_08_07_165628_create_certificates_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up() {
+        Schema::create('certificates', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->string('certificate_url'); // Putanja do sertifikata (PDF ili sl.)
+            $table->timestamps();
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('certificates');
+    }
+};
