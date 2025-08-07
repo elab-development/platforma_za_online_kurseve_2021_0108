fbcdbbfb61941aef70f1b381a8d1291d8809a091
Natasa Matovic <nacamatovic47@gmail.com>
2025-08-07 18:56:07 +0200

    Migracija enrollements

---
+++ back_end/database/migrations/2025_08_07_165538_create_enrollments_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up() {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('enrollments');
+    }
+};
