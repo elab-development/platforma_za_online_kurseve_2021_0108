6e0444d22eae713b945554368295e31c28cec522
Veeljko02 <veljkopetrovic02@gmail.com>
2025-08-13 23:39:37 +0200

    Dodati hookovi

---
+++ front_end/src/hooks/useAuth.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+const useAuth = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const login = (username, role) => {
+    const userData = { username, role };
+    localStorage.setItem("user", JSON.stringify(userData));
+    setUser(userData);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+  };
+
+  return { user, login, logout };
+};
+
+export default useAuth;
