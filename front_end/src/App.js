f9e277a332f09114fa56187a71d2373285cafb84
Natasa Matovic <nacamatovic47@gmail.com>
2025-08-14 17:50:58 +0200

    promene

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ForgotPassword from "./components/ForgotPassword";
+import Dashboard from "./components/Dashboard";
+import AddCourse from "./components/AddCourse";
+import CertificatesPage from "./components/CertificatesPage";
+import { AuthProvider, AuthContext } from "./context/AuthContext";
+import CoursesList from "./components/CoursesList";
+import Courses from "./components/Courses";
+import Settings from "./components/Settings";
+import AllUsers from "./components/AllUsers";
+
+const ProtectedRoute = ({ element }) => {
+  const { user } = useContext(AuthContext);
+  return user ? element : <Navigate to="/" />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
+          <Route path="/add-course" element={<ProtectedRoute element={<AddCourse />} />} />
+          <Route path="/certificates" element={<ProtectedRoute element={<CertificatesPage />} />} />
+          <Route path="/courses-list" element={<ProtectedRoute element={<CoursesList />} />} />
+          <Route path="/courses" element={<ProtectedRoute element={<Courses />} />} />
+          <Route path="/settings" element={<ProtectedRoute element={<Settings />} />} />
+          <Route path="/all-users" element={<ProtectedRoute element={<AllUsers />} />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
