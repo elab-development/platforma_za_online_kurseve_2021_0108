aae7e1ff42f66eac754ed3c1e2268271f38d549d
Natasa Matovic <nacamatovic47@gmail.com>
2025-08-16 20:13:18 +0200

    wip: povezivanje klijenta i servera

--- back_end/bootstrap/app.php
+++ back_end/bootstrap/app.php
@@ -9,10 +9,18 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        api: __DIR__.'/../routes/api.php',
+        apiPrefix: 'api',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+         $exceptions->render(function (Throwable $e, $request) {
+        if ($request->expectsJson() || $request->is('api/*')) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], method_exists($e, 'getStatusCode') ? $e->getStatusCode() : 400);
+        }
+    });
     })->create();
